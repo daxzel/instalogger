578b2e3f15b708dfa98b618778eec82aec69c22c
daxzel <andreytsarevskiy@gmail.com>
2013-12-08 18:14:19 +0400

    idea plugin fix

--- integration/idea/src/com/instalogger/idea/ShowExceptionInvokeServer.java
+++ integration/idea/src/com/instalogger/idea/ShowExceptionInvokeServer.java
@@ -5,12 +5,12 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 
 /**
-* Created with IntelliJ IDEA.
-* User: andreytsarevskiy
-* Date: 07/12/13
-* Time: 14:08
-* To change this template use File | Settings | File Templates.
-*/
+ * Created with IntelliJ IDEA.
+ * User: andreytsarevskiy
+ * Date: 07/12/13
+ * Time: 14:08
+ * To change this template use File | Settings | File Templates.
+ */
 class ShowExceptionInvokeServer extends NanoHTTPD {
 
     public ShowExceptionInvokeServer() {
@@ -22,11 +22,18 @@ class ShowExceptionInvokeServer extends NanoHTTPD {
         String file = session.getParms().get("file");
         String line = session.getParms().get("line");
 
-        for(Project project : ProjectManager.getInstance().getOpenProjects()) {
+        for (Project project : ProjectManager.getInstance().getOpenProjects()) {
             ShowExceptionInvokeAction openAction = new ShowExceptionInvokeAction(project, file, Integer.parseInt(line));
             ApplicationManager.getApplication().invokeLater(openAction);
         }
-        return new Response(Response.Status.OK, MIME_PLAINTEXT, "");
+        Response response = new Response(Response.Status.OK, MIME_PLAINTEXT, "");
+
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+
+        return response;
     }
 
 }
